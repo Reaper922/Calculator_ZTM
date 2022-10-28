7df17d1facbf436ae20017a976e2976ec34be2d8
Dennis Ammen <dennis.ammen@outlook.com>
2022-10-28 23:02:37 +0200

    Basic logic

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
-const clear = document.getElementById('clear-btn');
+const clearBtn = document.getElementById('clear-btn');
 
 function sendNumberValue(number) {
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+}
+
+function addDecimal() {
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
 }
 
 inputBtns.forEach((button => {
@@ -13,6 +19,13 @@ inputBtns.forEach((button => {
     } else if (button.classList.contains('operator')) {
         button.addEventListener('click', () => sendNumberValue(button.value));
     } else if (button.classList.contains('decimal')) {
-        button.addEventListener('click', () => sendNumberValue(button.value));
+        button.addEventListener('click', addDecimal);
     }
 }))
+
+
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+clearBtn.addEventListener('click', resetAll);
